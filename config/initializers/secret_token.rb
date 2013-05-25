c51f4e3f20a925899be609276407eadb27292495
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2013-05-25 13:31:16 +0200

    Initial commit. Bootstrap navbar, Devise-generated User model, stub of CommutesController.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commuter::Application.config.secret_token = '43f57a855b7211d3be557e6c2605224782312f46e1e550d945f983f8538868c5d1a0884c7c9d6efa9ee5b6677ecc27df3c7f2b16551443f44195a4621e16d3fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Commuter::Application.config.session_store :cookie_store, key: '_commuter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Commuter::Application.config.session_store :active_record_store
