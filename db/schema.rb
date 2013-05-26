fbb22163258f61c30aee355542f377533be57f1e
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2013-05-26 22:04:15 +0200

    Added colouring to the commute index page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130525183626) do
+ActiveRecord::Schema.define(:version => 20130526195247) do
 
   create_table "commutes", :force => true do |t|
     t.integer  "driver_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20130525183626) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "colour"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
