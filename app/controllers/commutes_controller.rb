47dae56e324c52db284dfe85f9afbe0a82d1d237
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2013-05-30 17:06:26 +0200

    Day summaries are now sorted by date.

--- app/controllers/commutes_controller.rb
+++ app/controllers/commutes_controller.rb
@@ -11,7 +11,7 @@ class CommutesController < ApplicationController
   end
 
   def index
-    @commutes = Commute.all  
+    @commutes = Commute.all
     @users = User.all
     @day_summaries = DaySummary.for_date_range(Date.civil(2012,1,1), Date.civil(2014,1,1))
   end
--- app/models/day_summary.rb
+++ app/models/day_summary.rb
@@ -1,4 +1,15 @@
 class DaySummary
+  def self.for_date_range(start_date, end_date)
+    commutes_by_date = Commute.where(['date >= ? AND date <= ?', start_date, end_date])
+      .group_by(&:date)
+
+    summaries = []
+    commutes_by_date.each do |date, commutes|
+      summaries << DaySummary.new(commutes, date)
+    end
+    summaries.sort {|s1, s2| s1.date <=> s2.date}
+  end
+
   def initialize(commutes, date)
     @commutes = commutes 
     @date = date
@@ -14,16 +25,5 @@ class DaySummary
     end
 
     return nil
-  end
-
-  def self.for_date_range(start_date, end_date)
-    commutes_by_date = Commute.where(['date >= ? AND date <= ?', start_date, end_date])
-      .group_by(&:date)
-
-    summaries = []
-    commutes_by_date.each do |date, commutes|
-      summaries << DaySummary.new(commutes, date)
-    end
-    summaries
   end
 end
