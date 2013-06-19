488d86ca1aa2119b9542ab92fdf88960de69164d
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2013-06-19 18:30:51 +0200

    Fuel reports can now be edited.

--- app/controllers/fuel_reports_controller.rb
+++ app/controllers/fuel_reports_controller.rb
@@ -1,5 +1,6 @@
 class FuelReportsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :user_must_own_report, only: [:edit, :update]
 
   def index
     @reports = FuelReport.all
@@ -11,12 +12,34 @@ class FuelReportsController < ApplicationController
 
   def create
     @report = current_user.fuel_reports.build(params[:fuel_report])
-    if @report.save
+    try_save(@report)
+  end
+
+  def edit
+  end
+
+  def update
+    @report.update_attributes(params[:fuel_report])
+    try_save(@report)
+  end
+  
+  private
+
+  def user_must_own_report
+    @report = current_user.fuel_reports.find_by_id(params[:id])
+    if @report.nil?
+      flash[:alert] = 'The fuel report does not exist or you are not allowed to edit it.'
+      redirect_to fuel_reports_path
+    end
+  end
+  
+  def try_save(report)
+    if report.save
       flash[:notice] = "Fuel report saved"
       redirect_to fuel_reports_path
     else
-      unless @report.errors[:base].blank?
-        flash[:alert] = @report.errors[:base].join
+      unless report.errors[:base].blank?
+        flash[:alert] = report.errors[:base].join
       end
       render 'new'
     end
--- app/validators/fuel_report_validator.rb
+++ app/validators/fuel_report_validator.rb
@@ -4,7 +4,12 @@ class FuelReportValidator < ActiveModel::Validator
 
     # If reports r1 and r2 do *not* overlap, than r1.to < r2.from || r1.from > r2.to
     # The condition below is this one negated. record is r2.
-    if FuelReport.where(['user_id = ? AND "to" >= ? AND "from" <= ?', record.user_id, record.from, record.to]).exists?
+    reports = FuelReport.where(['user_id = ? AND "to" >= ? AND "from" <= ?', record.user_id, record.from, record.to])
+    unless record.new_record?
+      reports = reports.where(['id != ?', record.id])
+    end
+
+    if reports.exists?
       record.errors[:base] << "The new fuel report overlaps with one of your existing reports."
     end
   end
