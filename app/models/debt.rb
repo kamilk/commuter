f9b6cbbb0c9e11e773cd20fd8eaa52ce0c9d6bde
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2013-06-22 11:23:20 +0200

    Debts: amount owed by someone is now subtracted from amount owed to this person.

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -8,7 +8,10 @@ class Debt
       price_per_person = 60 * report.price_per_km / commute.number_of_people
       commute.users.each do |user|
         debts_by_user[user] = Debt.new if debts_by_user[user].nil?
-        debts_by_user[user].add(commute.driver, price_per_person)
+        debts_by_user[user].add_owed_to(commute.driver, price_per_person)
+
+        debts_by_user[commute.driver] = Debt.new if debts_by_user[commute.driver].nil?
+        debts_by_user[commute.driver].subtract_owed_by(user, price_per_person)
       end
     end
     debts_by_user
@@ -16,14 +19,21 @@ class Debt
 
   def initialize
     @debt_by_user = {}
+    @reverse_debt_by_user = {}
   end
 
-  def add(user, amount)
+  def add_owed_to(user, amount)
     @debt_by_user[user] = 0 if @debt_by_user[user].nil?
     @debt_by_user[user] += amount
   end
+
+  def subtract_owed_by(user, amount)
+    @reverse_debt_by_user[user] = 0 if @reverse_debt_by_user[user].nil?
+    @reverse_debt_by_user[user] += amount
+  end
   
   def owed_to(user)
-    @debt_by_user[user] || 0
+    result = (@debt_by_user[user] || 0) - (@reverse_debt_by_user[user] || 0)
+    return result >= 0 ? result : 0
   end
 end
